ce413eb67e0309e303551d2e83fcdde57895135e
Felix Nguyen <fvnguyen79@gmail.com>
2021-05-10 20:23:10 -0700

    set up some models for p1 and other files

---
+++ project1/src/main/java/models/Reimbursement.java
@@ -0,0 +1,132 @@
+package models;
+
+import java.io.Serializable;
+
+public class Reimbursement implements Serializable{
+
+	private int amount;
+	private String type;
+	private Boolean submitted;
+	private Boolean resolved;
+	private String desc;
+	private String receipt;
+	
+	public Reimbursement(int amount, String type, Boolean submitted, Boolean resolved, String desc, String receipt) {
+		super();
+		this.amount = amount;
+		this.type = type;
+		this.submitted = submitted;
+		this.resolved = resolved;
+		this.desc = desc;
+		this.receipt = receipt;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Boolean getSubmitted() {
+		return submitted;
+	}
+
+	public void setSubmitted(Boolean submitted) {
+		this.submitted = submitted;
+	}
+
+	public Boolean getResolved() {
+		return resolved;
+	}
+
+	public void setResolved(Boolean resolved) {
+		this.resolved = resolved;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getReceipt() {
+		return receipt;
+	}
+
+	public void setReceipt(String receipt) {
+		this.receipt = receipt;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + amount;
+		result = prime * result + ((desc == null) ? 0 : desc.hashCode());
+		result = prime * result + ((receipt == null) ? 0 : receipt.hashCode());
+		result = prime * result + ((resolved == null) ? 0 : resolved.hashCode());
+		result = prime * result + ((submitted == null) ? 0 : submitted.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Reimbursement other = (Reimbursement) obj;
+		if (amount != other.amount)
+			return false;
+		if (desc == null) {
+			if (other.desc != null)
+				return false;
+		} else if (!desc.equals(other.desc))
+			return false;
+		if (receipt == null) {
+			if (other.receipt != null)
+				return false;
+		} else if (!receipt.equals(other.receipt))
+			return false;
+		if (resolved == null) {
+			if (other.resolved != null)
+				return false;
+		} else if (!resolved.equals(other.resolved))
+			return false;
+		if (submitted == null) {
+			if (other.submitted != null)
+				return false;
+		} else if (!submitted.equals(other.submitted))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Reimbursement [amount=" + amount + ", type=" + type + ", submitted=" + submitted + ", resolved="
+				+ resolved + ", desc=" + desc + ", receipt=" + receipt + "]";
+	}
+	
+	
+	
+}
---
+++ project1/src/main/java/util/UtilConnection.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class UtilConnection {
+
+	private static Connection connection;
+	
+	public static Connection getConnectionFromEnv() throws SQLException
+	{
+
+		String url = System.getenv("DB_URL");
+		String username = System.getenv("DB_USER");
+		String password = System.getenv("DB_PASS");
+
+		if(connection == null || connection.isClosed()) {
+		connection = DriverManager.getConnection(url, username, password);
+		}
+		
+		return connection;
+	}
+}
