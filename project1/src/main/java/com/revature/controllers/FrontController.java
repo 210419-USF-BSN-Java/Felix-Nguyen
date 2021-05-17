1a908f2b83ea961a719b8432bf4c79839bbcb1d3
Felix Nguyen <fvnguyen79@gmail.com>
2021-05-16 20:21:37 -0700

    more html files incoming

--- project1/src/main/java/com/revature/controllers/FrontController.java
+++ project1/src/main/java/com/revature/controllers/FrontController.java
@@ -1,30 +1,36 @@
 package com.revature.controllers;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.catalina.servlets.DefaultServlet;
 
-public class FrontController extends HttpServlet{
+
+public class FrontController extends DefaultServlet{
 
 	private static final long serialVersionUID = 1L;
 
 	private RequestHelper rh = new RequestHelper();
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
-		PrintWriter pw = response.getWriter();
-		pw.println("hello");
-		/*String path = request.getRequestURI().substring(request.getContextPath().length());
+//		PrintWriter pw = response.getWriter();
+//		pw.println("hello");
+		String path = request.getRequestURI().substring(request.getContextPath().length());
+		System.out.println(request.getRequestURI());
+		System.out.println(request.getContextPath());
 		
 		if(path.startsWith("/static/") || path.equals("/") || path.equals("/index.html") ) {
 			super.doGet(request, response);
 		}else {
 			rh.processRequest(request, response);
-		}*/
+		}
+		
+		
 	}
 	
 
